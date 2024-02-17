96ed02805f3b08ceb0ad8e469fe0c96e62654317
Tomash Lishka <tl9242972@gmail.com>
2024-02-17 12:32:56 +0100

    hw-6-js

--- js/task-2.js
+++ js/task-2.js
@@ -1,36 +1,23 @@
-function calcAverageCalories(days) {
-  let averageCalories = 0;
-
-  for (let oneDay of days) {
-    averageCalories += oneDay.calories / 7;
+class Storage {
+  #items = [];
+  constructor(items) {
+    this.#items = [...items];
   }
-  return averageCalories;
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    return this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter(item => item !== itemToRemove);
+    return this.#items;
+  }
 }
 
-console.log(
-  calcAverageCalories([
-    { day: "monday", calories: 3010 },
-    { day: "tuesday", calories: 3200 },
-    { day: "wednesday", calories: 3120 },
-    { day: "thursday", calories: 2900 },
-    { day: "friday", calories: 3450 },
-    { day: "saturday", calories: 3280 },
-    { day: "sunday", calories: 3300 }
-  ])
-); // 3180
-
-console.log(
-  calcAverageCalories([
-    { day: "monday", calories: 2040 },
-    { day: "tuesday", calories: 2270 },
-    { day: "wednesday", calories: 2420 },
-    { day: "thursday", calories: 1900 },
-    { day: "friday", calories: 2370 },
-    { day: "saturday", calories: 2280 },
-    { day: "sunday", calories: 2610 }
-  ])
-); // 2270
-
-console.log(
-  calcAverageCalories([])
-); // 0
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
